fefa0c5debe31f9c65211069ad97673d0d6c7f85
kardelio <kardeliogithub@outlook.com>
2024-12-01 11:26:52 +0100

    Initial commit - day 1

---
+++ src/day1/day1.kt
@@ -0,0 +1,28 @@
+package day1
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    println("Start..")
+    val lines = getFile()
+    val listOne = mutableListOf<Int>()
+    val listTwo = mutableListOf<Int>()
+    lines.forEach {
+        val splitt = it.split("   ")
+        listOne.add(splitt[0].toInt())
+        listTwo.add(splitt[1].toInt())
+    }
+    println(listOne.sorted())
+    println(listTwo.sorted())
+    val done = listOne.sorted().zip(listTwo.sorted()).map {
+        println("${it.first} - ${it.second}")
+        val diff = it.first - it.second
+        println(abs(diff))
+        abs(diff)
+    }.sum()
+    println(done)
+}
+fun getFile(): List<String>{
+    return File("src/day1/input.txt").readLines()
+}
